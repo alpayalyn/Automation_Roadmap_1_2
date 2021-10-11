c795d864ffa7d675c7863c418758e657aab70816
Alpay Alın <alpay.aln@insider.local>
2021-10-11 13:35:34 +0300

    Second?

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
-print("Selamlar, hoşgeldiniz.")
-
-Numberinserted = int(input("Please insert a number which consists of 2 digits __"))
+while True:
 
 
-def sayiAtama(Numberinserted):
-    if ((Numberinserted >= 10) and (Numberinserted <= 99)):
-        sayiOkunusu(Numberinserted)
-        print("You entered valid answer.")
-    else:
-        print("You entered invalid answer.Please try again.")
+    exit = input("Type Y if you want to end the programme or N")
 
-def sayiOkunusu(Numberinserted):
+    if(exit == "Y"):
 
-    demet = {11: "on bir", 12: "on iki", 13: "on üç", 14: "on dört", 15: "on beş"}
-    print(demet[Numberinserted])
+        break
+
+    elif(exit == "N"):
+
+        Numberinserted = int(input("Please insert a number which consists of 2 digits:"))
+
+        def sayiAtama(Numberinserted):
+
+            if ((Numberinserted >= 10) and (Numberinserted <= 99)):
+                sayiOkunusu(Numberinserted)
+
+                print("You entered valid answer.")
+
+            else:
+                print("You entered invalid answer.Please try again.")
+
+        def sayiOkunusu(Numberinserted):
+
+            number = Numberinserted
+
+            numList = [int(digit) for digit in str(number)]
 
 
-sayiAtama(Numberinserted)
+
+            demet0 = {1: "on", 2: "yirmi", 3: "otuz", 4: "kırk", 5: "elli", 6: "altmış", 7: "yetmiş", 8: "seksen", 9: "doksan"}
+            birler = demet0[numList[0]]
 
 
+            demet1 = {1: "bir", 2: "iki", 3: "üç", 4: "dört", 5: "beş", 6: "altı", 7: "yedi", 8: "sekiz", 9: "dokuz"}
+            onlar = demet1[numList[1]]
+
+            print("{} {}".format(birler, onlar))
+
+        sayiAtama(Numberinserted)
 
 
