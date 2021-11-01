6072e247e3002eac854171f184a149272ca22cdf
alpayalyn <alpay.alin@useinsider.com>
2021-11-01 22:56:14 +0300

    Latest

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
-while True: #sürekli çalışmasın
+exit = input("Type Y if you want to end the programme or N") 
 
+if(exit == "Y"):
+    pass
 
-    exit = input("Type Y if you want to end the programme or N") 
+else:
+    Numberinserted = int(input("Please insert a number which consists of 2 digits:")) #camelCase
 
-    if(exit == "Y"):
+    def sayiAtama(Numberinserted):
 
-        break
+        if ((Numberinserted >= 10) and (Numberinserted <= 99)):
+            sayiOkunusu(Numberinserted)
+            print("You entered valid answer.")
 
-    elif(exit == "N"): #else
+        else:
+            print("You entered invalid answer.Please try again.")
 
-        Numberinserted = int(input("Please insert a number which consists of 2 digits:")) #camelCase
+    def sayiOkunusu(Numberinserted):
 
-        def sayiAtama(Numberinserted):
+        numList = [int(digit) for digit in str(Numberinserted)]
 
-            if ((Numberinserted >= 10) and (Numberinserted <= 99)):
-                sayiOkunusu(Numberinserted)
+        onesDigit = {1: "on", 2: "yirmi", 3: "otuz", 4: "kırk", 5: "elli", 6: "altmış", 7: "yetmiş", 8: "seksen", 9: "doksan"}
+        ones = onesDigit[numList[0]]
 
-                print("You entered valid answer.")
+        tensDigit = {1: "bir", 2: "iki", 3: "üç", 4: "dört", 5: "beş", 6: "altı", 7: "yedi", 8: "sekiz", 9: "dokuz"}
+        tens = tensDigit[numList[1]]
 
-            else:
-                print("You entered invalid answer.Please try again.")
-
-        def sayiOkunusu(Numberinserted):
-
-            number = Numberinserted #tek variable, silelim
-
-            numList = [int(digit) for digit in str(number)]
-
-
-
-            demet0 = {1: "on", 2: "yirmi", 3: "otuz", 4: "kırk", 5: "elli", 6: "altmış", 7: "yetmiş", 8: "seksen", 9: "doksan"}
-            birler = demet0[numList[0]]
-
-
-            demet1 = {1: "bir", 2: "iki", 3: "üç", 4: "dört", 5: "beş", 6: "altı", 7: "yedi", 8: "sekiz", 9: "dokuz"}
-            onlar = demet1[numList[1]]
-            #variable isimlerini düzeltelim
-            print("{} {}".format(birler, onlar))
-
-        sayiAtama(Numberinserted)
-            #refactor yapalım
+        print("{} {}".format(ones, tens))
+    sayiAtama(Numberinserted)
 
